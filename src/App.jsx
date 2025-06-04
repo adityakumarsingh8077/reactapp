10126fa42ab7e728b844bc0a67c831447d2f9fe0
adityakumarsingh8077 <singhaditya8077@gmail.com>
2025-06-04 20:18:28 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ function App() {
     <p className='outer'>
    <p className='inner1'> <p className='p1' >
 
-    <Pizza name='FarmHouuse'  ingredients='veggies,cheese'price={300} /></p>
-    <p className='p2'><Pizza name='FarmHouuse' ingredients='veggies,cheese'price={500} /></p></p>
-   <p className='inner2'> <p className='p3'><Pizza name='FarmHouuse' ingredients='veggies,cheese'price={700} /></p>
-    <p className='p4'><Pizza name='FarmHouuse' ingredients='veggies,cheese'price={700} /></p></p>
+    <Pizza name='FarmHouse'  ingredients='veggies,cheese'price={300} /></p>
+    <p className='p2'><Pizza name='FarmHouse' ingredients='veggies,cheese'price={500} /></p></p>
+   <p className='inner2'> <p className='p3'><Pizza name='FarmHouse' ingredients='veggies,cheese'price={700} /></p>
+    <p className='p4'><Pizza name='FarmHouse' ingredients='veggies,cheese'price={700} /></p></p>
     </p></>
   )
 }
 function Pizza(props){
   return(
     <>
-    {props.name}{props.ingredients}{props.price}
+     <p className='name' ><strong>Name:</strong> {props.name}</p>
+      <p  className='ingre' ><strong>Ingredients:</strong> {props.ingredients}</p>
+      <p  className='price' ><strong>Price:</strong> ₹{props.price}</p>
+    {/* {props.name}<br/>{props.ingredients}<br/>₹{props.price} */}
     </>
 )
   }
